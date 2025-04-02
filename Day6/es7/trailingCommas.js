0a03214f800e9dc33b2566c179039c1adb712d33
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-02 11:45:44 +0530

    [Avinash Yadav]: Day6: es7: Add : trailing commas

---
+++ Day6/es7/trailingCommas.js
@@ -0,0 +1,15 @@
+function myFunc(x,y,){
+    return x+y;
+}
+console.log(myFunc(3,5));
+
+let a=[2,4,6,7,];
+console.log(a);//[ 2, 4, 6, 7 ]
+
+let obj = {
+    name: "Avi",
+    age: 22,
+}; 
+console.log(obj); //{ name: 'Avi', age: 22 }
+
+
