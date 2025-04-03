7a74bd199134b75050ad01b57726ec09eeb7613b
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-03 11:26:29 +0530

    [Avinash Yadav]: Day7:  Added checking Data Types

---
+++ Day7/EmployeeWage/checkingDataTypes.js
@@ -0,0 +1,11 @@
+function checkType(value) {
+    return typeof value;
+}
+let a=1;
+console.log(checkType(a));
+let b="Avi";
+console.log(checkType(b));
+let c=true;
+console.log(checkType(c));
+let d={name:"Avinash"};
+console.log(checkType(d));
