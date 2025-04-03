06414e4ec2424a38b5dff507c24d2905f6f5e96a
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-03 15:12:27 +0530

    [Avinash Yadav] : Day7 : Added REPL folder: Implemented REPL functions and its working

---
+++ Day7/REPL/repl.js
@@ -0,0 +1,24 @@
+function add(a,b){return a+b}
+function add(a,b){return a+b;}
+add(2,3)
+5+2
+add(2,3)
+_*2
+if(true){
+console.log("Avinash")
+}
+if(true){
+return 2
+if(true){
+return 2;}
+Math.sqrt(4)
+function add(a,b){return a+b}
+function add(a,b){return a+b;}
+add(2,3)
+5+2
+add(2,3)
+_*2
+if(true){
+console.log("Avinash")
+}
+Math.sqrt(4)
