048ea2ccf62d8fa6e8e56d8b1bc8b382e4a9bb00
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-04 14:45:28 +0530

    [Avinash Yadav] : Day2 : Modify add Element

--- Day2/Arrays/addElement.js
+++ Day2/Arrays/addElement.js
@@ -1,7 +1,10 @@
 let a =[10,5,7,8,3];
-a.splice(2,0,100,120);
+a.splice(2,1,100,120); 
+/*
+in splice (index to start inserting, how much element to delete, which all elements to add)
+*/
 console.log(a);
-a.splice(2,1,200,400);
+a.splice(2,2,200,400);
 console.log(a);
 a.splice(1,3,170,180);
 console.log(a);
