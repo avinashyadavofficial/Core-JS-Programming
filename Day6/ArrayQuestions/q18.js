42d3db9493100161bd76a884db9d4cad6968ec23
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-04 16:07:33 +0530

    [Avinash Yadav] : Day6 : Modify q18

--- Day6/ArrayQuestions/q18.js
+++ Day6/ArrayQuestions/q18.js
@@ -8,3 +8,13 @@ console.log(ct);
 //way2
 let b=a.reduce((acc,item)=>item=='apple'?acc+1:acc,0);
 console.log(b);
+
+//way3
+let count=0;
+a.forEach(sol);
+function sol(value){
+    if(value=='apple'){
+        count+=1;
+    }
+}
+console.log(count);
